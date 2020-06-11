f47923bc25e32877dd431e366863c44bf077e783
IgorPelowski <i.z.pelowski@gmail.com>
2020-06-11 22:31:53 +0200

    object pies has been added

--- javascript1.js
+++ javascript1.js
@@ -3,3 +3,11 @@ const color = document.querySelector('.siatkarze-kurek_tekst');
 color.style.backgroundColor = 'green'}
 
 colorr() ;
+
+const pies = {
+    nazwa: 'piesek',
+    waga: 20,
+    dlugosc: 100,
+}
+
+console.log(pies)
